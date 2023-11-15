0e95b9bbfbf093dede8e275258b87fd24793e902
MrAntonio7 <antonioalvarezmalagon@gmail.com>
2023-11-15 12:29:25 +0100

    Commit Script2

--- Assets/script2.cs
+++ Assets/script2.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script2 : MonoBehaviour
+public class Script2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Carlos");
+        Debug.Log("Hola");
     }
 
     // Update is called once per frame
